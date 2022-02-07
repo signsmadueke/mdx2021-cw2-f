b84dc870180f31c2b08f5b6fbe03a3c1b1701b75
signsmadueke <signsmadueke1@gmail.com>
2022-02-07 08:29:38 +0100

    initial commit

---
+++ js/lessons.js
@@ -0,0 +1,92 @@
+let lessons = [
+    {
+        id: 1001,
+        subject: 'Math',
+        location: 'London',
+        price: 170,
+        numOfSpaces: 5,
+        image: 'image/calc.png',
+        icon: 'fa fa-calculator'
+    },
+    {
+        id: 1002,
+        subject: 'English',
+        location: 'Cardiff',
+        price: 160,
+        numOfSpaces: 5,
+        image: 'image/english.jpg',
+        icon: 'fa fa-book'
+    },
+    {
+        id: 1003,
+        subject: 'Science',
+        location: 'Hendon',
+        price: 10,
+        numOfSpaces: 5,
+        image: 'image/science.jpg',
+        icon: 'fa fa-briefcase'
+    },
+    {
+        id: 1004,
+        subject: 'Business',
+        location: 'America',
+        price: 50,
+        numOfSpaces: 5,
+        image: 'image/business.jpg',
+        icon: 'fa fa-folder-open'
+    },
+    {
+        id: 1005,
+        subject: 'ICT',
+        location: 'Ibadan',
+        price: 900,
+        numOfSpaces: 5,
+        image: 'image/computer.jpg',
+        icon: 'fa fa-desktop'
+    },
+    {
+        id: 1006,
+        subject: 'Physics',
+        location: 'Lagos',
+        price: 150,
+        numOfSpaces: 5,
+        image: 'image/physics.jpg',
+        icon: 'fa fa-cubes'
+    },
+    {
+        id: 1007,
+        subject: 'Music',
+        location: 'Kano',
+        price: 120,
+        numOfSpaces: 5,
+        image: 'image/music.jpg',
+        icon: 'fa fa-music'
+    },
+    {
+        id: 1008,
+        subject: 'French',
+        location: 'Katsina',
+        price: 600,
+        numOfSpaces: 5,
+        image: 'image/pentool.png',
+        icon: 'fa fa-graduation-cap'
+    },
+    {
+        id: 1009,
+        subject: 'Christian Religious Studies',
+        location: 'London',
+        price: 400,
+        numOfSpaces: 5,
+        image: 'image/crs.png',
+        icon: 'fa fa-university'
+    },
+    {
+        id: 1010,
+        subject: 'Chemistry',
+        location: 'London',
+        price: 200,
+        numOfSpaces: 5,
+        image: 'image/chemistry.jpg',
+        icon: 'fa fa-thermometer-three-quarters'
+    },
+]
