cd547edeae70719ae8b1cc515aac2942212b8233
signsmadueke <signsmadueke1@gmail.com>
2022-02-07 08:44:46 +0100

    ...

--- js/lessons.js
+++ js/lessons.js
@@ -1,92 +1,89 @@
-let lessons = [
-    {
-        id: 1001,
-        subject: 'Math',
-        location: 'London',
-        price: 170,
-        numOfSpaces: 5,
-        image: 'image/calc.png',
-        icon: 'fa fa-calculator'
+let lessons = [{
+        id: 1,
+        subject: "Accounting",
+        location: "	California",
+        price: 250,
+        spaces: 5,
+        image: "images/lessons/accounting.jpg"
     },
     {
-        id: 1002,
-        subject: 'English',
-        location: 'Cardiff',
-        price: 160,
-        numOfSpaces: 5,
-        image: 'image/english.jpg',
-        icon: 'fa fa-book'
+        id: 2,
+        subject: "Biology",
+        location: "Texas",
+        price: 433,
+        spaces: 5,
+        image: "images/lessons/biology.jpg"
     },
     {
-        id: 1003,
-        subject: 'Science',
-        location: 'Hendon',
-        price: 10,
-        numOfSpaces: 5,
-        image: 'image/science.jpg',
-        icon: 'fa fa-briefcase'
+        id: 3,
+        subject: "Chemistry",
+        location: "California",
+        price: 534,
+        spaces: 5,
+        image: "images/lessons/chemistry.jpg"
+    },
+    // {
+    //     id: 4,
+    //     subject: "Commerce",
+    //     location: "New York",
+    //     price: 211,
+    //     spaces: 5,
+    //     image: "images/lessons/commerce.jpg"
+    // },
+    {
+        id: 5,
+        subject: "Computer",
+        location: "Florida",
+        price: 455,
+        spaces: 5,
+        image: "images/lessons/computer.jpg"
     },
     {
-        id: 1004,
-        subject: 'Business',
-        location: 'America',
-        price: 50,
-        numOfSpaces: 5,
-        image: 'image/business.jpg',
-        icon: 'fa fa-folder-open'
+        id: 6,
+        subject: "Economics",
+        location: "Washington",
+        price: 656,
+        spaces: 5,
+        image: "images/lessons/economics.jpg"
     },
     {
-        id: 1005,
-        subject: 'ICT',
-        location: 'Ibadan',
-        price: 900,
-        numOfSpaces: 5,
-        image: 'image/computer.jpg',
-        icon: 'fa fa-desktop'
+        id: 7,
+        subject: "English",
+        location: "Pennsylvania",
+        price: 322,
+        spaces: 5,
+        image: "images/lessons/english.jpg"
     },
     {
-        id: 1006,
-        subject: 'Physics',
-        location: 'Lagos',
-        price: 150,
-        numOfSpaces: 5,
-        image: 'image/physics.jpg',
-        icon: 'fa fa-cubes'
+        id: 8,
+        subject: "Geography",
+        location: "New Jersey",
+        price: 434,
+        spaces: 5,
+        image: "images/lessons/geography.jpg"
     },
     {
-        id: 1007,
-        subject: 'Music',
-        location: 'Kano',
-        price: 120,
-        numOfSpaces: 5,
-        image: 'image/music.jpg',
-        icon: 'fa fa-music'
+        id: 9,
+        subject: "Mathematics",
+        location: "Maryland",
+        price: 989,
+        spaces: 5,
+        image: "images/lessons/maths.jpg"
     },
     {
-        id: 1008,
-        subject: 'French',
-        location: 'Katsina',
-        price: 600,
-        numOfSpaces: 5,
-        image: 'image/pentool.png',
-        icon: 'fa fa-graduation-cap'
+        id: 10,
+        subject: "Music",
+        location: "Connecticut",
+        price: 534,
+        spaces: 5,
+        image: "images/lessons/music.jpg"
     },
     {
-        id: 1009,
-        subject: 'Christian Religious Studies',
-        location: 'London',
-        price: 400,
-        numOfSpaces: 5,
-        image: 'image/crs.png',
-        icon: 'fa fa-university'
-    },
-    {
-        id: 1010,
-        subject: 'Chemistry',
-        location: 'London',
-        price: 200,
-        numOfSpaces: 5,
-        image: 'image/chemistry.jpg',
-        icon: 'fa fa-thermometer-three-quarters'
+        id: 11,
+        subject: "Physics",
+        location: "Massachusetts",
+        price: 432,
+        spaces: 5,
+        image: "images/lessons/physics.jpg"
     },
 ]
