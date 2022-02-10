9ea10390a446edcb7d6a00d378652d1dbe322674
signsmadueke <signsmadueke1@gmail.com>
2022-02-10 18:58:29 +0100

    Added PWA apps

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+var cacheName = "petstore-v1";
+var cacheFiles = [
+    'index.html',
+    'petstore.webmanifest',
+    'images/accounting.jpg',
+    'images/biology.jpg',
+    'images/chemistry.jpg',
+    'images/commerce.jpg',
+    'images/economics.jpg',
+    'images/english.jpg',
+    'images/geography.jpg',
+    'images/maths.jpg',
+    'images/music.jpg',
+    'images/physics.jpg',
+    'images/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        cache.match(e.request).then(function (r) {
+            //Download the file if it is not in the cache
+            return r || fetch(e.request).then(function (response) {
+                //Add the new file to cache
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                })
+            })
+        })
+    )
+});
